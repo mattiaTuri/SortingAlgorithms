510f9f14c3a79ba7e031820c115e2d8ed135c17c
Mattia Turina <mattia.turina@enerj.it>
2023-09-05 17:13:47 +0200

    added animation to insertion sort

--- src/algorithms/insertionSort.ts
+++ src/algorithms/insertionSort.ts
@@ -1,34 +1,58 @@
 export const insertionSort = async (time:number) => {
     let spanElemList: NodeListOf<HTMLSpanElement> =
     document.querySelectorAll("#container span");
+    let spanElem: HTMLSpanElement[] = Array.from(spanElemList);
 
     for(let i:number = 1; i < spanElemList.length; i++){        
         for(let j:number = 0; j < spanElemList.length; j++){
             let currentNumber = spanElemList[i]
             if(currentNumber == undefined) break;
             let n = j
+            updateElemPos(currentNumber, "0", "-100")
+            let initialElementDistance:number = 0
             while(n >= 0){
                 let prevNumber = spanElemList[n]
-                highlighElem(prevNumber, currentNumber, "red")
-                await delay(time);
+                //highlighElem(prevNumber, currentNumber, "red")
+                await delay(time);            
                 if(parseInt(currentNumber.id) < parseInt(prevNumber.id)){
-                    document.getElementById("container")?.insertBefore(currentNumber, prevNumber);
+                    const diff:number = currentNumber.offsetLeft - prevNumber.offsetLeft
+                    updateElemPos(prevNumber, diff.toString(), "0")
+                    await delay(time);   
+                    prevNumber.style.transitionDuration = "0ms"
+                    prevNumber.style.transform = "translate(0,0)"
+                    currentNumber.style.transform = "300ms"
+                    currentNumber.style.transform = `translate(${diff + initialElementDistance}px,-100px)`
+                    //currentNumber.style.transform = `translate(${diff}px, -100px)`
+                    prevNumber.before(currentNumber)
+                    //document.getElementById("container")?.insertBefore(currentNumber, prevNumber);
                     spanElemList = document.querySelectorAll("#container span");
-                    highlighElem(prevNumber, currentNumber, "#faa916")
+                    //highlighElem(prevNumber, currentNumber, "#faa916")
                     n--
+                    initialElementDistance += diff
                 }else{
-                    highlighElem(prevNumber, currentNumber, "#faa916")
+                    //updateElemPos(currentNumber, "0", "0")
                     break;
+                    //highlighElem(prevNumber, currentNumber, "#faa916")
                 }
-                await delay(time);
+
             }
             i++
+            await delay(time);
+            //const diff = currentNumber.offsetLeft - prevNumber.offsetLeft
+            updateElemPos(currentNumber, "0", "-100")
+            await delay(time);
+            updateElemPos(currentNumber, "0", "0")
             await delay(time);
         }
         break;
     }
 
     document.querySelectorAll("#container span").forEach((elem:any) => elem.style.backgroundColor = "#20FC8F");
+}
+
+const updateElemPos = (number: HTMLSpanElement, x:string, y:string) => {
+    number.style.transitionDuration = "300ms"
+    number.style.transform = `translate(${x}px, ${y}px)`
 }
 
 const highlighElem = (prevNumber: HTMLSpanElement,
