9437ca26272c7bc2cf51e1f9dd784f3a1867e31e
mattiaTuri <turina.mattia@gmail.com>
2023-09-06 15:42:03 +0200

    fix insertion sort smartphone animation

--- src/algorithms/insertionSort.ts
+++ src/algorithms/insertionSort.ts
@@ -8,40 +8,32 @@ export const insertionSort = async (time:number) => {
             let currentNumber = spanElemList[i]
             if(currentNumber == undefined) break;
             let n = j
-            updateElemPos(currentNumber, "0", "-100")
+            window.innerWidth < 1024 ? highlighElem(currentNumber, "red") : updateElemPos(currentNumber, "0", "-100", "300ms")
             let initialElementDistance:number = 0
             while(n >= 0){
                 let prevNumber = spanElemList[n]
-                //highlighElem(prevNumber, currentNumber, "red")
                 await delay(time);            
                 if(parseInt(currentNumber.id) < parseInt(prevNumber.id)){
                     const diff:number = currentNumber.offsetLeft - prevNumber.offsetLeft
-                    updateElemPos(prevNumber, diff.toString(), "0")
-                    await delay(time);   
-                    prevNumber.style.transitionDuration = "0ms"
-                    prevNumber.style.transform = "translate(0,0)"
-                    currentNumber.style.transform = "300ms"
-                    currentNumber.style.transform = `translate(${diff + initialElementDistance}px,-100px)`
-                    //currentNumber.style.transform = `translate(${diff}px, -100px)`
+                    window.innerWidth < 1024 ? highlighElem(prevNumber, "red") : updateElemPos(prevNumber, diff.toString(), "0", "300ms")
+                    await delay(time);  
+                    const res = diff + initialElementDistance
+                    window.innerWidth < 1024 ? highlighElem(prevNumber, "#faa916") : 1024 && updateElemPos(prevNumber, "0", "0", "0ms")
+                    window.innerWidth >= 1024 && updateElemPos(currentNumber, res.toString(), "-100", "300ms")
                     prevNumber.before(currentNumber)
-                    //document.getElementById("container")?.insertBefore(currentNumber, prevNumber);
                     spanElemList = document.querySelectorAll("#container span");
-                    //highlighElem(prevNumber, currentNumber, "#faa916")
                     n--
                     initialElementDistance += diff
                 }else{
-                    //updateElemPos(currentNumber, "0", "0")
                     break;
-                    //highlighElem(prevNumber, currentNumber, "#faa916")
                 }
 
             }
             i++
             await delay(time);
-            //const diff = currentNumber.offsetLeft - prevNumber.offsetLeft
-            updateElemPos(currentNumber, "0", "-100")
+            window.innerWidth < 1024 ? highlighElem(currentNumber, "#faa916") : updateElemPos(currentNumber, "0", "-100", "300ms")
             await delay(time);
-            updateElemPos(currentNumber, "0", "0")
+            window.innerWidth < 1024 ? highlighElem(currentNumber, "#faa916") : updateElemPos(currentNumber, "0", "0", "300ms")
             await delay(time);
         }
         break;
@@ -50,14 +42,12 @@ export const insertionSort = async (time:number) => {
     document.querySelectorAll("#container span").forEach((elem:any) => elem.style.backgroundColor = "#20FC8F");
 }
 
-const updateElemPos = (number: HTMLSpanElement, x:string, y:string) => {
-    number.style.transitionDuration = "300ms"
+const updateElemPos = (number: HTMLSpanElement, x:string, y:string, time:string) => {
+    number.style.transitionDuration = time
     number.style.transform = `translate(${x}px, ${y}px)`
 }
 
-const highlighElem = (prevNumber: HTMLSpanElement,
-    currentNumber: HTMLSpanElement, color:string) => {
-      prevNumber.style.backgroundColor = color;
+const highlighElem = (currentNumber: HTMLSpanElement, color:string) => {
       currentNumber.style.backgroundColor = color;
 }
 
